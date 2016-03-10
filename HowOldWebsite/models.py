b8d6c91cfd839e67a043f925c7457c0e2ce899bd
Yu Hao <jinyu121@126.com>
2016-03-10 21:50:19 +0800

    Outline of Models

--- HowOldWebsite/models.py
+++ HowOldWebsite/models.py
@@ -1,1 +1,47 @@
-# Create your models here.
+# -*- coding: UTF-8 -*-
+
+from django.db import models
+
+
+# ===== 数据库记录项 =====
+
+class RecordOriginalImage(models.Model):
+    # 原始大图片
+    pass
+
+
+class RecordFace(models.Model):
+    # 检测出来的人脸
+    pass
+
+
+class RecordSex(models.Model):
+    # 检测出来的性别
+    pass
+
+
+class RecordAge(models.Model):
+    # 检测出来的年龄
+    pass
+
+
+class RecordSmile(models.Model):
+    # 检测出来的微笑程度
+    pass
+
+
+# ===== 模型记录 =====
+
+class ModelSex(models.Model):
+    # 性别模型
+    pass
+
+
+class ModelAge(models.Model):
+    # 年龄模型
+    pass
+
+
+class ModelSmile(models.Model):
+    # 微笑模型
+    pass
