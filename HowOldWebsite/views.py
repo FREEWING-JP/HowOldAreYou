2d69931c573afedb856b4d1995db3c4132c02176
Yu Hao <jinyu121@126.com>
2016-03-12 17:13:50 +0800

    Hello, View!

--- HowOldWebsite/views.py
+++ HowOldWebsite/views.py
@@ -1,1 +1,10 @@
-# Create your views here.
+# -*- coding: UTF-8 -*-
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello Index!")
+
+
+def result(request):
+    return HttpResponse("Hello Result!")
