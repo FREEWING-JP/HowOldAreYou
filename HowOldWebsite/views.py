f71145614549a73447a12640ece80d98cb6e94db
Yu Hao <jinyu121@126.com>
2016-03-15 22:36:51 +0800

    Hello, File Upload~

--- HowOldWebsite/views.py
+++ HowOldWebsite/views.py
@@ -17,6 +17,7 @@ def index(request):
 
 
 def fisher(request):
+    result = {}
     if not request.method == "POST":
         response_data = {'success': False,
                          'message': 'Must in POST method'
@@ -27,49 +28,75 @@ def fisher(request):
     image_file = request.FILES.getlist('file')
 
     # print('Save The Image ...')
+
+    result_upload = False
+    imag_upload = False
+
     if not image_url == '':
-        fetch_image_by_url(image_url, request)
+        (result_upload, imag_upload) = fetch_image_by_url(image_url, request)
     elif len(image_file) >= 1:
-        fetch_image_by_uploading(image_file, request)
+        (result_upload, imag_upload) = fetch_image_by_uploading(image_file, request)
     else:
         response_data = {'success': False,
                          'message': 'No image'
                          }
         return HttpResponse(json.dumps(response_data))
 
+    if not result_upload:
+        response_data = {'success': False,
+                         'message': 'Upload Failed'
+                         }
+        return HttpResponse(json.dumps(response_data))
+
+    result['image'] = imag_upload
+
     print('Detect Face ...')
     print('Predict Sex ...')
     print('Predict Age ...')
     print('Predict Smile ...')
-    return HttpResponse("Hello Fisher!")
+    return HttpResponse("Hello, Fisher")
 
 
 def fetch_image_by_uploading(file, request):
     # print('Save image by upload')
-    pic_id = uuid.uuid4()
-    os.path.join(BASE_DIR, "upload")
-    image = file[0]
-    record_original_image = RecordOriginalImage(id=pic_id,
-                                                upload_time=datetime.today(),
-                                                source_url='USER UPLOAD',
-                                                user_ip=request.META['REMOTE_ADDR'],
-                                                size_x=0,
-                                                size_y=0,
-                                                size_scale=0,
-                                                used_flag=0
-                                                )
+    try:
+        image_file = file[0]
+        pic_id = uuid.uuid4()
+        filename = str(pic_id) + '.jpg'
+        upload_filename = os.path.join(BASE_DIR, "upload", filename)
+        with open(upload_filename, 'wb+') as destination:
+            for chunk in image_file.chunks():
+                destination.write(chunk)
+        record_original_image = RecordOriginalImage(id=pic_id,
+                                                    upload_time=datetime.today(),
+                                                    source_url='USER UPLOAD',
+                                                    user_ip=request.META['REMOTE_ADDR'],
+                                                    size_x=0,
+                                                    size_y=0,
+                                                    size_scale=0,
+                                                    used_flag=0
+                                                    )
+        record_original_image.save()
+        return (True, record_original_image)
+    except:
+        return (False, False)
 
 
 def fetch_image_by_url(url, request):
-    print('Save image by url')
-    pic_id = uuid.uuid4()
-    os.path.join(BASE_DIR, "upload")
-    record_original_image = RecordOriginalImage(id=pic_id,
-                                                upload_time=datetime.today(),
-                                                source_url=url,
-                                                user_ip=request.META['REMOTE_ADDR'],
-                                                size_x=0,
-                                                size_y=0,
-                                                size_scale=0,
-                                                used_flag=0
-                                                )
+    # print('Save image by url')
+    try:
+        pic_id = uuid.uuid4()
+        os.path.join(BASE_DIR, "upload")
+        record_original_image = RecordOriginalImage(id=pic_id,
+                                                    upload_time=datetime.today(),
+                                                    source_url=url,
+                                                    user_ip=request.META['REMOTE_ADDR'],
+                                                    size_x=0,
+                                                    size_y=0,
+                                                    size_scale=0,
+                                                    used_flag=0
+                                                    )
+        record_original_image.save()
+        return (True, record_original_image)
+    except:
+        return (False, False)
