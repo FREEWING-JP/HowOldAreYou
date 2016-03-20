a71500825090356975e7b8fd6f3ee5f1c951bc0b
Yu Hao <jinyu121@126.com>
2016-03-20 10:30:46 +0800

    Add favicon.ico to prevent 404 error in log

--- HowOldWebsite/urls.py
+++ HowOldWebsite/urls.py
@@ -1,9 +1,14 @@
 # -*- coding: UTF-8 -*-
 from django.conf.urls import url
+from django.views.generic.base import RedirectView
 
 from . import views
 
 urlpatterns = [
+    # favicon.ico
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/favicon.ico')),
+    # the index page
     url(r'^$', views.index, name='index'),
+    # the main processor
     url(r'^fisher$', views.fisher, name='fisher'),
 ]
